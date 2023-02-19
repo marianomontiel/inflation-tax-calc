e6e9908d0c3d77d75c911412a913861df76f16f7
marianomontiel <67610069+marianomontiel@users.noreply.github.com>
2023-02-19 13:12:34 -0300

    Add calculateMinimumWage()

--- script.js
+++ script.js
@@ -115,21 +115,8 @@ function monthList(a) {
     });
   }
 }
-
-function calculateTax() {
-  //convert form into Array
-  const salaryForm = document.querySelectorAll(".input-salary");
-  const salaryFormData = new FormData();
-  salaryForm.forEach((salaryForm) => {
-    salaryFormData.append(salaryForm.name, salaryForm.value);
-  });
-  let salaryArray = Array.from(salaryFormData.values());
-  //convert Array input to number
-  salaryArray = salaryArray.map(function (v) {
-    return parseInt(v) || 0;
-  });
-  //inflation array database
-  const inflationTable = [
+//inflation array database
+const inflationTable = [
     { Mes: 1, Año: 2017, Index: 1.6 },
     { Mes: 2, Año: 2017, Index: 2.1 },
     { Mes: 3, Año: 2017, Index: 2.4 },
@@ -204,12 +191,101 @@ function calculateTax() {
     { Mes: 12, Año: 2022, Index: 5.1 },
     { Mes: 1, Año: 2023, Index: 6.0 }
   ];
-  //filter array to desired period
+const minimumWage = [
+  { Mes: 01, Año: 2017, Sueldo: 8060 },
+  { Mes: 02, Año: 2017, Sueldo: 8060 },
+  { Mes: 03, Año: 2017, Sueldo: 8060 },
+  { Mes: 04, Año: 2017, Sueldo: 8060 },
+  { Mes: 05, Año: 2017, Sueldo: 8060 },
+  { Mes: 06, Año: 2017, Sueldo: 8860 },
+  { Mes: 07, Año: 2017, Sueldo: 8860 },
+  { Mes: 08, Año: 2017, Sueldo: 8860 },
+  { Mes: 09, Año: 2017, Sueldo: 8860 },
+  { Mes: 10, Año: 2017, Sueldo: 8860 },
+  { Mes: 11, Año: 2017, Sueldo: 8860 },
+  { Mes: 12, Año: 2017, Sueldo: 9500 },
+  { Mes: 01, Año: 2018, Sueldo: 9500 },
+  { Mes: 02, Año: 2018, Sueldo: 9500 },
+  { Mes: 03, Año: 2018, Sueldo: 9500 },
+  { Mes: 04, Año: 2018, Sueldo: 9500 },
+  { Mes: 05, Año: 2018, Sueldo: 9500 },
+  { Mes: 06, Año: 2018, Sueldo: 10000 },
+  { Mes: 07, Año: 2018, Sueldo: 10000 },
+  { Mes: 08, Año: 2018, Sueldo: 10700 },
+  { Mes: 09, Año: 2018, Sueldo: 10700 },
+  { Mes: 10, Año: 2018, Sueldo: 10700 },
+  { Mes: 11, Año: 2018, Sueldo: 11300 },
+  { Mes: 12, Año: 2018, Sueldo: 11300 },
+  { Mes: 01, Año: 2019, Sueldo: 11300 },
+  { Mes: 02, Año: 2019, Sueldo: 12500 },
+  { Mes: 03, Año: 2019, Sueldo: 12500 },
+  { Mes: 04, Año: 2019, Sueldo: 12500 },
+  { Mes: 05, Año: 2019, Sueldo: 12500 },
+  { Mes: 06, Año: 2019, Sueldo: 12500 },
+  { Mes: 07, Año: 2019, Sueldo: 14125 },
+  { Mes: 08, Año: 2019, Sueldo: 15625 },
+  { Mes: 09, Año: 2019, Sueldo: 16875 },
+  { Mes: 10, Año: 2019, Sueldo: 16875 },
+  { Mes: 11, Año: 2019, Sueldo: 16875 },
+  { Mes: 12, Año: 2019, Sueldo: 16875 },
+  { Mes: 01, Año: 2020, Sueldo: 16875 },
+  { Mes: 02, Año: 2020, Sueldo: 16875 },
+  { Mes: 03, Año: 2020, Sueldo: 16875 },
+  { Mes: 04, Año: 2020, Sueldo: 16875 },
+  { Mes: 05, Año: 2020, Sueldo: 16875 },
+  { Mes: 06, Año: 2020, Sueldo: 16875 },
+  { Mes: 07, Año: 2020, Sueldo: 16875 },
+  { Mes: 08, Año: 2020, Sueldo: 16875 },
+  { Mes: 09, Año: 2020, Sueldo: 18900 },
+  { Mes: 10, Año: 2020, Sueldo: 18900 },
+  { Mes: 11, Año: 2020, Sueldo: 20587.5 },
+  { Mes: 12, Año: 2020, Sueldo: 20587.5 },
+  { Mes: 01, Año: 2021, Sueldo: 20587.5 },
+  { Mes: 02, Año: 2021, Sueldo: 21600 },
+  { Mes: 03, Año: 2021, Sueldo: 23544 },
+  { Mes: 04, Año: 2021, Sueldo: 24408 },
+  { Mes: 05, Año: 2021, Sueldo: 25272 },
+  { Mes: 06, Año: 2021, Sueldo: 27216 },
+  { Mes: 07, Año: 2021, Sueldo: 28080 },
+  { Mes: 08, Año: 2021, Sueldo: 31104 },
+  { Mes: 09, Año: 2021, Sueldo: 32000 },
+  { Mes: 10, Año: 2021, Sueldo: 32000 },
+  { Mes: 11, Año: 2021, Sueldo: 32000 },
+  { Mes: 12, Año: 2021, Sueldo: 32000 },
+  { Mes: 01, Año: 2022, Sueldo: 33000 },
+  { Mes: 02, Año: 2022, Sueldo: 33000 },
+  { Mes: 03, Año: 2022, Sueldo: 38940 },
+  { Mes: 04, Año: 2022, Sueldo: 38940 },
+  { Mes: 05, Año: 2022, Sueldo: 45540 },
+  { Mes: 06, Año: 2022, Sueldo: 45540 },
+  { Mes: 07, Año: 2022, Sueldo: 47850 },
+  { Mes: 08, Año: 2022, Sueldo: 51200 },
+  { Mes: 09, Año: 2022, Sueldo: 54550 },
+  { Mes: 10, Año: 2022, Sueldo: 57900 },
+  { Mes: 11, Año: 2022, Sueldo: 61953 },
+  { Mes: 12, Año: 2022, Sueldo: 65427 },
+  { Mes: 01, Año: 2023, Sueldo: 67743 },
+  { Mes: 02, Año: 2023, Sueldo: 69500 }
+]
 
+function calculateTax() {
+  //convert form into Array
+  const salaryForm = document.querySelectorAll(".input-salary");
+  const salaryFormData = new FormData();
+  salaryForm.forEach((salaryForm) => {
+    salaryFormData.append(salaryForm.name, salaryForm.value);
+  });
+  let salaryArray = Array.from(salaryFormData.values());
+  //convert Array input to number
+  salaryArray = salaryArray.map(function (v) {
+    return parseInt(v) || 0;
+  });
+    
+  //filter array to desired period
   inflationTable.forEach(function (element) {
-  element.date = element.Mes + element.Año * 12;
-});
-  
+    element.date = element.Mes + element.Año * 12;
+  });
+
   const startDate = startMonth + startYear * 12;
   const endDate = endMonth + endYear * 12;
   const filterTable = inflationTable.filter(
@@ -248,6 +324,68 @@ function calculateTax() {
   const output = document.querySelector(".output");
   output.setAttribute("style", "padding-top: 0px");
   output.innerText = `Tu salario perdió el equivalente a $ ${Math.floor((sumWithInitial2 - sumWithInitial) * 100) / 100
+    } en poder de compra durante este periodo.`;
+  box.appendChild(output);
+}
+
+function calculateMinimumWage() {
+  //convert form into Array
+  minimumWage.forEach(function (element) {
+    element.date = element.Mes + element.Año * 12;
+  });
+
+  const startDate = startMonth + startYear * 12;
+  const endDate = endMonth + endYear * 12;
+  const wageFiltered= minimumWage.filter(
+    (element) =>
+      element.date >= startDate &&
+      element.date <= endDate
+  );
+
+  const wageMapped = wageFiltered.map((value) => value.Sueldo);
+  console.table(wageMapped);
+    
+  //filter array to desired period
+  inflationTable.forEach(function (element) {
+    element.date = element.Mes + element.Año * 12;
+  });
+
+  const filterTable = inflationTable.filter(
+    (element) =>
+      element.date >= startDate &&
+      element.date <= endDate
+  );
+
+  //return array of inflation values
+  const inflationArray = filterTable.map((index) => index.Index);
+  //create inflation salary index
+  let inflationIndex = Array(inflationArray.length);
+  let salaryAdjusted = Array(inflationArray.length);
+  for (let i = 0, length = inflationArray.length; i < length; i++) {
+    if (i < 1) {
+      inflationIndex[0] = inflationArray[0] / 100 + 1;
+    } else {
+      inflationIndex[i] = inflationIndex[i - 1] * (inflationArray[i] / 100 + 1);
+    }
+    salaryAdjusted[i] = inflationIndex[i] * wageMapped[i];
+  }
+  //subtract salary adjusted with original salary
+  const initialValue = 0;
+  const sumWithInitial = wageMapped.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+
+  const sumWithInitial2 = salaryAdjusted.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+
+  //output value on DOM
+  const box = document.querySelector(".box");
+  const output = document.querySelector(".output");
+  output.setAttribute("style", "padding-top: 0px");
+  output.innerText = `El salario minimo vital y movil perdió el equivalente a $ ${Math.floor((sumWithInitial2 - sumWithInitial) * 100) / 100
     } en poder de compra durante este periodo.`;
   box.appendChild(output);
 }
